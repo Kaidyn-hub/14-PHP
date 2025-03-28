b581916a9e7a2ae5aefd2786e027714bbdecf463
Kaidyn-hub <sindoaija02@gmail.com>
2025-03-28 16:04:26 +0800

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -24,35 +24,35 @@
     <table>
         <tr>
             <td width="120">Age: </td>
-            <td style="text-decoration: underline">
+            <td>
                 <!-- Use ternary operator to check if the request type is GET or POST -->
                 <?php echo $_POST['age']; ?>
             </td>
         </tr>
         <tr>
             <td>Gender: </td>
-            <td style="text-decoration: underline">
+            <td>
                 <!-- Use ternary operator to check if the request type is GET or POST -->
                 <?php echo $_POST['gender']; ?>
             </td>
         </tr>
         <tr>
             <td>Email:</td>
-            <td style="text-decoration: underline">
+            <td>
                 <!-- Use ternary operator to check if the request type is GET or POST -->
                 <?php echo $_POST['email'];?>
             </td>
         </tr>
         <tr>
             <td>Address: </td>
-            <td style="text-decoration: underline">
+            <td>
                 <!-- Use ternary operator to check if the request type is GET or POST -->
                 <?php echo $_POST['address']; ?>
             </td>
         </tr>
         <tr>
             <td>Contact Number:</td>
-            <td style="text-decoration: underline">
+            <td>
                 <!-- Use ternary operator to check if the request type is GET or POST -->
                 <?php echo $_POST['no'];?>
             </td>
